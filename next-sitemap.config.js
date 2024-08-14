3ce81be7f20a7615c9b9ab486f20ff32c2426c6d
Philip Laxamana <philip.j.laxamana@gmail.com>
2024-08-14 19:18:31 -0400

    added next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://philiplaxamana.com',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
